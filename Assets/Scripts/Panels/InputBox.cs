39663a01bf5a9b84b0ba6fa425be780a100118e4
wajahatdeveloper <wajahatdeveloperqs@gmail.com>
2021-04-28 15:14:50 +0500

    Added Input Box and Auto Anchor Point Setter

---
+++ Assets/Scripts/Panels/InputBox.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class InputBox : SingletonBehaviour<InputBox>
+{
+    public Text headingText;
+    public InputField inputField;
+    public GameObject inputPanel;
+
+    public string enteredText = ""; 
+
+    public UnityEvent<string> onClose;
+
+    public void Show(string message)
+    {
+        headingText.text = message;
+        inputPanel.SetActive(true);
+    }
+
+    public void OnClick_Submit()
+    {
+        enteredText = inputField.text;
+        Hide();
+    }
+
+    public void Hide()
+    {
+        inputPanel.SetActive(false);
+        onClose?.Invoke(enteredText);
+        onClose?.RemoveAllListeners();
+    }
+}
--- Assets/Scripts/Panels/PopupMessage.cs
+++ Assets/Scripts/Panels/PopupMessage.cs
@@ -13,14 +13,29 @@ public class PopupMessage : SingletonBehaviour<PopupMessage>
 
     public UnityEvent onClose;
 
+    private bool _isAuto = false;
+
+    public void ShowAuto(string message)
+    {
+        _isAuto = true;
+        Show(message);
+    }
+    
     public void Show(string message)
     {
         messageText.text = message;
         messagePanel.SetActive(true);
     }
 
+    public void HideAuto()
+    {
+        _isAuto = false;
+        Hide();
+    }
+    
     public void Hide()
     {
+        if (_isAuto) { return; }
         messagePanel.SetActive(false);
         onClose?.Invoke();
         onClose?.RemoveAllListeners();
