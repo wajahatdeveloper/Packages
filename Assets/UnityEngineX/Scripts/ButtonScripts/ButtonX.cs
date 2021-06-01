883723e29e66a8ec531ef939a617ffe7562074a8
WAJAHAT\Dell <wajahatdeveloperqs@gmail.com>
2021-06-01 02:02:58 -0700

    Added Edito Console Pro and Modified ButtonX

--- Assets/UnityEngineX/Scripts/ButtonScripts/ButtonX.cs
+++ Assets/UnityEngineX/Scripts/ButtonScripts/ButtonX.cs
@@ -1,25 +1,52 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.Serialization;
 using UnityEngine.UI;
 
 namespace UnityEngineX
 {
-	public class ButtonX : MonoBehaviour
+	[RequireComponent(typeof(Button))]
+	public class ButtonX : MonoBehaviour , IPointerEnterHandler , IPointerExitHandler , IPointerDownHandler , IPointerUpHandler
 	{
+		[Foldout("Click Action",true)]
+		public UnityEvent<ButtonX> OnClick;
+		
 		[Foldout("Click Sound",true)]
 		public bool clickSound;
 		public int soundID;
 		public bool useCustomSound;
 		[ConditionalField( "useCustomSound" )]
-			public AudioClip customSound;
+		public AudioClip customSound;
 
-		private Button button;
+		[Foldout("Click Events", true)]
+		public UnityEvent<ButtonX> OnPointerDown;
+		public UnityEvent<ButtonX> OnPointerUp;
+		public UnityEvent<ButtonX> OnPointerEnter;
+		public UnityEvent<ButtonX> OnPointerExit;
+
+		public object Data
+		{
+			get => _data;
+			set => _data = value;
+		}
+		
+		public Button Button
+		{
+			get => _button;
+			set => _button = value;
+		}
+
+		private object _data;
+		private Button _button;
 
 		private void Start()
 		{
-			button = GetComponent<Button>();
-			button.onClick.AddListener( () => {
+			_button = GetComponent<Button>();
+			_button.onClick.AddListener( () => {
+				OnClick?.Invoke(this);
 				if (clickSound)
 				{
 					if (useCustomSound)
@@ -32,6 +59,26 @@ namespace UnityEngineX
 					}
 				}
 			} );
+		}
+
+		void IPointerEnterHandler.OnPointerEnter(PointerEventData eventData)
+		{
+			OnPointerEnter?.Invoke(this);
+		}
+
+		void IPointerExitHandler.OnPointerExit(PointerEventData eventData)
+		{
+			OnPointerExit?.Invoke(this);
+		}
+
+		void IPointerDownHandler.OnPointerDown(PointerEventData eventData)
+		{
+			OnPointerDown?.Invoke(this);
+		}
+
+		void IPointerUpHandler.OnPointerUp(PointerEventData eventData)
+		{
+			OnPointerUp?.Invoke(this);
 		}
 	}
 }
