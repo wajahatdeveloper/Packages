88b0aba94d0d32322804d5d3d3ec59ca6f27ea68
WAJAHAT\Dell <wajahatdeveloperqs@gmail.com>
2021-06-01 04:09:51 -0700

    Modified Build Manager

--- Assets/UnityEngineX/Scripts/BuildManager/BuildManager.cs
+++ Assets/UnityEngineX/Scripts/BuildManager/BuildManager.cs
@@ -1,12 +1,27 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+
+#if UNITY_EDITOR
+    using UnityEditor;
+#endif
 
 namespace UnityEngineX
 {
     public class BuildManager : MonoBehaviour
     {
+#if UNITY_EDITOR
+        public Vector4 buildNumber = Vector4.zero;
+
+        private void OnValidate()
+        {
+            if (buildNumber == Vector4.zero) return;
+            var bundleVersion = $"{buildNumber.x}.{buildNumber.y}.{buildNumber.z}.{buildNumber.w}";
+            if (PlayerSettings.bundleVersion == bundleVersion) return;
+            PlayerSettings.bundleVersion = bundleVersion;
+            Debug.Log("Version Changed to " + PlayerSettings.bundleVersion);
+        }
+#endif
+
         public string StoredBuildVersion
         {
             set => PlayerPrefs.SetString("buildVersion", value);
