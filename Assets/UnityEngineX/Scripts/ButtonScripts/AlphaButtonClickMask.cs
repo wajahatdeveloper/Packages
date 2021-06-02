2f335dad4340b8d7dd1daa604741305f07e84c4e
WAJAHAT\Dell <wajahatdeveloperqs@gmail.com>
2021-06-02 00:15:04 -0700

    Converted Currency Manager to Economy Manager

--- Assets/UnityEngineX/Scripts/ButtonScripts/AlphaButtonClickMask.cs
+++ Assets/UnityEngineX/Scripts/ButtonScripts/AlphaButtonClickMask.cs
@@ -10,11 +10,10 @@ public class AlphaButtonClickMask : MonoBehaviour, ICanvasRaycastFilter
     {
         _image = GetComponent<Image>();
 
-        Texture2D tex = _image.sprite.texture as Texture2D;
+        var tex = _image.sprite.texture;
 
-        bool isInvalid = false;
+        var isInvalid = false;
         if (tex != null)
-        {
             try
             {
                 tex.GetPixels32();
@@ -24,16 +23,10 @@ public class AlphaButtonClickMask : MonoBehaviour, ICanvasRaycastFilter
                 Debug.LogError(e.Message);
                 isInvalid = true;
             }
-        }
         else
-        {
             isInvalid = true;
-        }
 
-        if (isInvalid)
-        {
-            Debug.LogError("This script need an Image with a readbale Texture2D to work.");
-        }
+        if (isInvalid) Debug.LogError("This script need an Image with a readbale Texture2D to work.");
     }
 
     public bool IsRaycastLocationValid(Vector2 sp, Camera eventCamera)
@@ -41,18 +34,19 @@ public class AlphaButtonClickMask : MonoBehaviour, ICanvasRaycastFilter
         Vector2 localPoint;
         RectTransformUtility.ScreenPointToLocalPointInRectangle(_image.rectTransform, sp, eventCamera, out localPoint);
 
-		Vector2 pivot = _image.rectTransform.pivot;
-		Vector2 normalizedLocal = new Vector2(pivot.x + localPoint.x / _image.rectTransform.rect.width, pivot.y + localPoint.y / _image.rectTransform.rect.height);
-        Vector2 uv = new Vector2(
-            _image.sprite.rect.x + normalizedLocal.x * _image.sprite.rect.width, 
-            _image.sprite.rect.y + normalizedLocal.y * _image.sprite.rect.height );
+        var pivot = _image.rectTransform.pivot;
+        var normalizedLocal = new Vector2(pivot.x + localPoint.x / _image.rectTransform.rect.width,
+            pivot.y + localPoint.y / _image.rectTransform.rect.height);
+        var uv = new Vector2(
+            _image.sprite.rect.x + normalizedLocal.x * _image.sprite.rect.width,
+            _image.sprite.rect.y + normalizedLocal.y * _image.sprite.rect.height);
 
         uv.x /= _image.sprite.texture.width;
         uv.y /= _image.sprite.texture.height;
 
         //uv are inversed, as 0,0 or the rect transform seem to be upper right, then going negativ toward lower left...
-        Color c = _image.sprite.texture.GetPixelBilinear(uv.x, uv.y);
+        var c = _image.sprite.texture.GetPixelBilinear(uv.x, uv.y);
 
-        return c.a> 0.1f;
+        return c.a > 0.1f;
     }
 }
---
+++ Assets/UnityEngineX/Scripts/EconomyManager/EconomyManager.cs
@@ -0,0 +1,63 @@
+﻿using ByteSheep.Events;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngineX;
+
+[System.Serializable] public class EconomyEvent : QuickEvent<string> { }
+
+public class EconomyManager : SingletonBehaviour<EconomyManager>
+{
+	public List<string> typesOfEnonomies;
+	[Space(16)]
+	public EconomyEvent onEconomyIncrease;
+	[Space(16)]
+	public EconomyEvent onEconomyDecrease;
+	[Space(16)]
+	public EconomyEvent onEconomyChange;
+
+	private const string CurrencyIdString = "currency_";
+	private Dictionary<string, int> _currencies = new Dictionary<string, int>();
+
+	private void Start()
+	{
+		foreach (var currency in typesOfEnonomies) { LoadEconomyData( currency ); }
+	}
+
+	public void IncreaseEconomyValue(string economyType, int amount)
+	{
+		_currencies[economyType] += amount;
+		onEconomyIncrease?.Invoke( economyType );
+		SaveEconomyData( economyType );
+	}
+
+	public void DecreaseEconomyValue( string economyType, int amount )
+	{
+		_currencies[economyType] -= amount;
+		onEconomyDecrease?.Invoke( economyType );
+		SaveEconomyData( economyType );
+	}
+
+	public void SetEconomyValue(string economyType, int amount)
+	{
+		_currencies[economyType] = amount;
+		onEconomyChange?.Invoke(economyType);
+		SaveEconomyData(economyType);
+	}
+
+	public int GetEconomyValue(string economyType)
+	{
+		return _currencies[economyType];
+	}
+
+	private void SaveEconomyData(string economyType)
+	{
+		PlayerPrefs.SetInt( CurrencyIdString + economyType, _currencies[economyType] );
+	}
+
+	private void LoadEconomyData( string economyType )
+	{
+		_currencies[economyType] = PlayerPrefs.GetInt( CurrencyIdString + economyType, 0 );
+	}
+}
