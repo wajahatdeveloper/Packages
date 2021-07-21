4f7ec4e515424dac848130d491ca318ce3b470a8
wajahat developer <wajahatdeveloperqs@gmail.com>
2021-07-21 19:33:59 +0500

    Renamed Sheet Codes to GameDB and Moved Its menu from Tools to Hub and updated feature list

---
+++ Assets/GameDB/Scripts/Attributes/ColumnName.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace GameDB
+{
+    [AttributeUsage(AttributeTargets.Field)]
+    public class ColumnName : Attribute
+    {
+        public readonly string columnName;
+
+        public ColumnName(string columnName)
+        {
+            this.columnName = columnName;
+        }
+    }
+}
