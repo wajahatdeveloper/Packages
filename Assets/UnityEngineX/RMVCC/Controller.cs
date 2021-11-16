197a5c59a2cd0a75ba2b9c8a5b3dc9337b02d0c6
Microperts <56877813+microperts@users.noreply.github.com>
2021-11-16 10:48:43 +0500

    Updates RMVCC

--- Assets/UnityEngineX/RMVCC/Controller.cs
+++ Assets/UnityEngineX/RMVCC/Controller.cs
@@ -1,7 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using Sirenix.OdinInspector;
 using UnityEngine;
 
-public abstract class Controller : MonoBehaviour
+public abstract class Controller<M,V> : RuleBehaviour
+    where M : Model
+    where V : View
 {
+    [Required] public V view;
+    [Required] public M model;
 }
--- Assets/UnityEngineX/RMVCC/Root.cs
+++ Assets/UnityEngineX/RMVCC/Root.cs
@@ -1,7 +1,35 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Sirenix.OdinInspector;
 using UnityEngine;
 
-public abstract class Root : MonoBehaviour
+public abstract class Root<M,V,C> : SingletonBehaviour<Root<M,V,C>>
+    where M : Model
+    where V : View
+    where C : Controller<M,V>
 {
+    [Required] public M Model;
+    [Required] public C Controller;
+    [Required] public V View;
+
+    [HideLabel, TextArea]
+    public string Description;
+
+    private void OnEnable()
+    {
+        Debug.Log("Entered Gameplay Scene");
+        
+        InjectMockData();
+    }
+
+    private void OnDisable()
+    {
+        Debug.Log("Left Gameplay Scene");
+    }
+
+    protected virtual void InjectMockData()
+    {
+        Debug.Log("Mock Data Injected");
+    }
 }
