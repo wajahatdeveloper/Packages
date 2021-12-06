9191eea14c5fb96aecdbfab89d7b5371869c9490
WajahatDeveloper <wajahatdeveloperqs@gmail.com>
2021-12-06 14:46:09 +0500

    Upgraded Wait Panel

--- Assets/User Interface Widgets/Component_Scripts/Panel_Scripts/WaitPanel.cs
+++ Assets/User Interface Widgets/Component_Scripts/Panel_Scripts/WaitPanel.cs
@@ -4,16 +4,49 @@ using UnityEngine.Events;
 public class WaitPanel : SingletonBehaviour<WaitPanel>
 {
     public GameObject waitPanel;
-
     public UnityEvent onClose;
 
+    private int _count = 0;
+    
     public void Show()
     {
-        waitPanel.SetActive(true);
+        if (waitPanel == null)
+        {
+            Debug.LogError("Wait panel game object not assigned");
+        }
+        else
+        {
+            Debug.Log("Wait Panel Shown");
+            waitPanel.SetActive(true);
+        }
+    }
+    
+    public void ShowCounted()
+    {
+        if (waitPanel == null)
+        {
+            Debug.LogError("Wait panel game object not assigned");
+        }
+        else
+        {
+            _count++;
+            Debug.Log("Wait Panel Shown : count = " + _count);
+            waitPanel.SetActive(true);
+        }
     }
 
+    public void HideCounted()
+    {
+        _count--;
+        if (_count <= 0)
+        {
+            Hide();
+        }
+    }
+    
     public void Hide()
     {
+        Debug.Log("Wait Panel Hidden");
         waitPanel.SetActive(false);
         onClose?.Invoke();
         onClose?.RemoveAllListeners();
